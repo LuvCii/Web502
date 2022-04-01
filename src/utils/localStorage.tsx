425ebb7b6421e2d7495f28477549133c70ae4b64
LuvCii <tranminhdungggg@gmail.com>
2022-04-01 23:44:13 +0700

    local Storage

---
+++ src/utils/localStorage.tsx
@@ -0,0 +1,8 @@
+import { User } from "../types/User";
+export const authenticated = (user: User, next: () => void) => {
+    localStorage.setItem('user', JSON.stringify(user));
+    next();
+}
+export const isAuthenticate = () => {
+    if (!localStorage.getItem('user')) return;
+}
